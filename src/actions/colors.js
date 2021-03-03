23046a3db859d0053dccec31f5bf35f45c46241a
Max Perdomo <67365354+MaxPerdomo@users.noreply.github.com>
2021-03-03 17:58:00 -0430

    Add Redux actions

---
+++ src/actions/colors.js
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+
+/* Copyright © 2021 Max Perdomo
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+export const changeColor = (color, hex) => ({
+    type: 'CHANGE_COLOR',
+    color,
+    hex
+});
+
+export const changeAllColors = (colors) => ({
+    type: 'CHANGE_ALL_COLORS',
+    colors
+});
