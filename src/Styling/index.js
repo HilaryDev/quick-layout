e449ad4fdf7f4073420ff7d330b97386b8a666d2
Max Perdomo <67365354+MaxPerdomo@users.noreply.github.com>
2021-03-03 13:48:00 -0430

    Add reusable styles

---
+++ src/Styling/index.js
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+
+/* Copyright © 2021 Max Perdomo
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+export {
+    COLORS,
+    Heading,
+    SubHeading,
+    HeadingLight,
+    Paragraph,
+    Button,
+    QUERIES
+} from './Styling';
