8445d2fbc662873bdfaa959baa05fd64a68b2437
Max Perdomo <67365354+MaxPerdomo@users.noreply.github.com>
2021-03-03 15:59:00 -0430

    Add Redux store

---
+++ src/store/buildData.js
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+
+/* Copyright © 2021 Max Perdomo
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+const buildData = {
+    first: [
+        {
+            type: 'nav'
+        },
+        {
+            type: 'triDiv'
+        },
+        {
+            type: 'div'
+        },
+        {
+            type: 'splitDiv'
+        }
+    ],
+    second: [
+        {
+            type: 'nav'
+        },
+        {
+            type: 'triDiv'
+        }
+    ],
+    third: [
+        {
+            type: 'nav'
+        }
+    ],
+    blank: []
+};
+
+export default buildData;
---
+++ src/store/configureStore.js
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+
+/* Copyright © 2021 Max Perdomo
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { createStore, combineReducers } from 'redux';
+
+import colorsReducer from '../reducers/colors';
+import preferencesReducer from '../reducers/preferences';
+
+
+export default () => {
+    // Create global store
+    const store = createStore(
+        combineReducers({
+            colors: colorsReducer,
+            preferences: preferencesReducer
+        }),
+    );
+
+    return store;
+};
