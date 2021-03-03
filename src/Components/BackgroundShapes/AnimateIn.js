0d47aed4401432f3e1bcc052454111e13386d870
Max Perdomo <67365354+MaxPerdomo@users.noreply.github.com>
2021-03-03 17:11:00 -0430

    Add background shapes

---
+++ src/Components/BackgroundShapes/AnimateIn.js
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+
+/* Copyright © 2021 Max Perdomo
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { keyframes } from 'styled-components/macro';
+
+
+const AnimateIn = keyframes`
+0%{
+    opacity: 0;
+    transform: translate(0, 10px) scale(0) rotate(0deg);
+}
+
+70%{
+    opacity: 1;
+    transform: translate(0, 0) scale(1.9) rotate(180deg);
+}
+
+100%{
+    opacity: 1;
+    transform: translate(0, 0) scale(1.7) rotate(200deg);
+}
+`;
+
+
+export default AnimateIn;
---
+++ src/Components/BackgroundShapes/index.js
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: MPL-2.0 */
+
+/* Copyright © 2021 Max Perdomo
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import React from 'react';
+
+import { Triangle, Circle, Dot } from './Shapes';
+import { COLORS } from '../../Styling';
+
+
+function Shapes() {
+    return (
+        <>
+            <Circle
+                topPosition="25%"
+                leftPosition="10%"
+                colorStroke={COLORS.palePink}
+            />
+            <Triangle colorStroke={COLORS.yellow} />
+            <Triangle
+                topPosition="85%"
+                leftPosition="25%"
+                colorStroke={COLORS.palePink}
+            />
+            <Triangle
+                topPosition="95%"
+                leftPosition="5%"
+                colorStroke={COLORS.paleBlue}
+            />
+            <Dot colorStroke={COLORS.paleBlue} />
+            <Circle colorStroke={COLORS.yellow} />
+        </>
+    );
+}
+
+export default Shapes;
